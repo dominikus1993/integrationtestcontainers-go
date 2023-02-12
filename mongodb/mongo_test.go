5fbbf05efdee076b11d7cd2a3e8ccf796978c010
Dominik.Kotecki <dominikus1910@gmail.com>
2023-02-12 09:13:07 +0000

    fixx

--- mongodb/mongo_test.go
+++ mongodb/mongo_test.go
@@ -37,15 +37,6 @@ func TestGetIdsThatExistsInDatabase(t *testing.T) {
 		t.Fatal(fmt.Errorf("error creating mongo client: %w", err))
 	}
 
-	t.Cleanup(func() {
-		if err := mongoClient.Disconnect(ctx); err != nil {
-			t.Fatalf("failed to disconnect mongo client: %s", err)
-		}
-	})
-
-	t.Run("Ping", func(t *testing.T) {
-		// Act
-		subject := mongoClient.Ping(ctx, nil)
-		assert.NoError(t, subject)
-	})
+	subject := mongoClient.Ping(ctx, nil)
+	assert.NoError(t, subject)
 }
