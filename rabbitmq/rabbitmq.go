186c7f065ff6ad79bf5492e0366df6393ba71902
Dominik.Kotecki <dominik.kotecki@rossmann.com.pl>
2025-08-13 12:12:52 +0200

    Refactor RabbitMQ container implementation to use GenericContainer and update type naming

--- rabbitmq/rabbitmq.go
+++ rabbitmq/rabbitmq.go
@@ -6,29 +6,36 @@ import (
 
 	"github.com/docker/go-connections/nat"
 	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/modules/rabbitmq"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 //var sql = []string{"/opt/mssql-tools/bin/sqlcmd", "-b", "-r", "1", "-U", config.username, "-P", config.password, "-i"}
 
-type rabbitmqContainer struct {
+type msSqlContainer struct {
 	testcontainers.Container
 	config *RabbitMqContainerConfiguration
 }
 
-func StartContainer(ctx context.Context, config *RabbitMqContainerConfiguration) (*rabbitmqContainer, error) {
-	container, err := rabbitmq.Run(ctx,
-		config.image,
-		rabbitmq.WithAdminUsername(config.username),
-		rabbitmq.WithAdminPassword(config.password),
-	)
+func StartContainer(ctx context.Context, config *RabbitMqContainerConfiguration) (*msSqlContainer, error) {
+	req := testcontainers.ContainerRequest{
+		Image:        config.image,
+		ExposedPorts: []string{fmt.Sprintf("%d/tcp", config.exposedPort)},
+		Env:          map[string]string{"RABBITMQ_DEFAULT_USER": config.username, "RABBITMQ_DEFAULT_PASS": config.password},
+		WaitingFor: wait.ForAll(
+			wait.ForLog("Server startup complete"),
+		),
+	}
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
 	if err != nil {
 		return nil, err
 	}
-	return &rabbitmqContainer{container, config}, nil
+	return &msSqlContainer{container, config}, nil
 }
 
-func (container *rabbitmqContainer) ConnectionString(ctx context.Context) (string, error) {
+func (container *msSqlContainer) ConnectionString(ctx context.Context) (string, error) {
 	mappedPort, err := container.MappedPort(ctx, nat.Port(fmt.Sprint(container.config.port)))
 	if err != nil {
 		return "", err
